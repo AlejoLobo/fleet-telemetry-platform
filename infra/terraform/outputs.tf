a3568486960bc2700f72cb10c3f31307d0ee2e8e
Alejandro Lobo-Guerrero <alejolobo.ti@gmail.com>
2026-07-09 18:55:11 -0500

    chore(terraform): ampliar blueprint con ecr y task definitions

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -10,6 +10,22 @@ output "ecs_cluster_name" {
   value = aws_ecs_cluster.fleet.name
 }
 
+output "ecr_api_repository_url" {
+  value = aws_ecr_repository.api.repository_url
+}
+
+output "ecr_worker_repository_url" {
+  value = aws_ecr_repository.worker.repository_url
+}
+
+output "ecs_task_definition_api_arn" {
+  value = aws_ecs_task_definition.api.arn
+}
+
+output "ecs_task_definition_worker_arn" {
+  value = aws_ecs_task_definition.worker.arn
+}
+
 output "public_subnet_ids" {
   value = aws_subnet.public[*].id
 }
