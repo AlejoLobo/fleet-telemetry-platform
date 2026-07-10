183da18243197208778cb6b4eef843fbb9aaadbe
Alejandro <219790614+AlejoLobo@users.noreply.github.com>
2026-07-10 16:24:07 -0500

    fix(worker): respetar limite maximo del backoff

--- backend/FleetTelemetry.Worker.Tests/KafkaProcessingRetryBackoffTests.cs
+++ backend/FleetTelemetry.Worker.Tests/KafkaProcessingRetryBackoffTests.cs
@@ -5,30 +5,30 @@ namespace FleetTelemetry.Worker.Tests;
 public class KafkaProcessingRetryBackoffTests
 {
     [Fact]
-    public void First_attempt_uses_initial_delay_plus_jitter_within_bound()
+    public void First_attempt_stays_within_max()
     {
         var random = new Random(42);
         var delay = KafkaProcessingRetryBackoff.ComputeDelay(1, 500, 5000, random);
-
-        Assert.InRange(delay.TotalMilliseconds, 500, 500 + 500 / 4);
+        Assert.InRange(delay.TotalMilliseconds, 500, 5000);
     }
 
     [Fact]
-    public void Exponential_growth_is_capped_at_max_delay_plus_jitter()
+    public void High_attempt_never_exceeds_max_including_jitter()
     {
         var random = new Random(7);
-        var delay = KafkaProcessingRetryBackoff.ComputeDelay(10, 500, 5000, random);
-
-        Assert.InRange(delay.TotalMilliseconds, 5000, 5000 + 5000 / 4);
+        for (var i = 0; i < 100; i++)
+        {
+            var delay = KafkaProcessingRetryBackoff.ComputeDelay(20, 500, 5000, random);
+            Assert.True(delay.TotalMilliseconds <= 5000, $"delay={delay.TotalMilliseconds}");
+        }
     }
 
     [Fact]
-    public void Second_attempt_doubles_base_before_jitter()
+    public void Second_attempt_grows_but_stays_within_max()
     {
         var random = new Random(0);
         var delay = KafkaProcessingRetryBackoff.ComputeDelay(2, 500, 5000, random);
-
-        Assert.InRange(delay.TotalMilliseconds, 1000, 1000 + 1000 / 4);
+        Assert.InRange(delay.TotalMilliseconds, 1000, 5000);
     }
 
     [Theory]
@@ -38,5 +38,12 @@ public class KafkaProcessingRetryBackoffTests
     {
         Assert.Throws<ArgumentOutOfRangeException>(() =>
             KafkaProcessingRetryBackoff.ComputeDelay(attempt, 500, 5000, new Random(1)));
+    }
+
+    [Fact]
+    public void Initial_greater_than_max_throws()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() =>
+            KafkaProcessingRetryBackoff.ComputeDelay(1, 6000, 5000, new Random(1)));
     }
 }
