a4c2be1a9b84c31da94b390345d3b558513d0ac9
Alejandro Lobo-Guerrero <alejolobo.ti@gmail.com>
2026-07-11 05:10:00 +0000

    feat(observabilidad): integrar trazas y métricas OTLP
    Co-authored-by: Alejandro Lobo-Guerrero <alejolobo.ti@gmail.com>

--- backend/FleetTelemetry.Worker/Program.cs
+++ backend/FleetTelemetry.Worker/Program.cs
@@ -1,12 +1,16 @@
 using FleetTelemetry.Infrastructure;
 using FleetTelemetry.Infrastructure.Configuration;
+using FleetTelemetry.Infrastructure.Observability;
 using FleetTelemetry.Worker;
 
 var builder = Host.CreateApplicationBuilder(args);
 
+builder.Logging.AddFleetOpenTelemetryLogging(builder.Configuration, InfrastructureProfile.Worker);
+
 ConfigurationValidator.Validate(builder.Configuration, builder.Environment);
 
 builder.Services.AddInfrastructure(builder.Configuration, InfrastructureProfile.Worker);
+builder.Services.AddFleetOpenTelemetry(builder.Configuration, InfrastructureProfile.Worker);
 builder.Services.AddSingleton<TelemetryMessageProcessor>();
 builder.Services.AddSingleton<TelemetryMessageCoordinator>();
 builder.Services.AddHostedService<TelemetryConsumerWorker>();
